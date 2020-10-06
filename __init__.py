43eeff9290c3188d46596aaa506528b239784268
calacuda <eowest@gmail.com>
2020-10-05 20:05:23 -0400

    alpha

--- __init__.py
+++ __init__.py
@@ -1,26 +1,17 @@
 from adapt.intent import IntentBuilder 
 from mycroft import MycroftSkill, intent_handler
-from os import system
+from os import system as run
 
-run = system
 
 class Launcher(MycroftSkill):
+
     def __init__(self):
         super().__init__()
 
     def initialize(self):
         self.register_intent("launch.intent", self.handle_launch_intent)
         self.register_entity("app.entity")
-        self.set_settings()
-
-    def set_settings(self):
-        #browser = self.settings.get('browser')
-        #terminal = self.settings.get('terminal')
-        apps = self.settings.get("Applications")
         
-    def on_settings_changed(self):
-        self.set_settings()
-
     @intent_handler('launch.intent')
     def handle_launch_intent(self, app):
         self.speak(f"app is {app}")
@@ -34,3 +25,5 @@ class Launcher(MycroftSkill):
 
 def create_skill():
     return Launcher()
+
+create_skill()
