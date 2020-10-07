7c6f6b8fd71c10fc8c46bcb19549bb9562e49bae
calacuda <eowest@gmail.com>
2020-10-07 10:11:11 -0400

    fixing chat client

--- __init__.py
+++ __init__.py
@@ -28,7 +28,7 @@ class Launcher(MycroftSkill):
     def get_target_app(self, app_title):
         #run(f"notify-send 'Error' '{app_title}'")
         app_name = self.equivilency(app_title)
-        #run(f"notify-send 'Error' '{app_name}'")
+        run(f"notify-send 'DEBUG' 'name :  {app_name}'")
         white_list = self.settings.get("white list").split(",")
         if app_title not in white_list and app_title in self.settings.keys():
             return self.settings.get(app_title)
