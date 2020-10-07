4ee00480319618f55b4de28646b15372ecdb858d
calacuda <eowest@gmail.com>
2020-10-07 05:22:23 -0400

    mvp

--- __init__.py
+++ __init__.py
@@ -14,16 +14,38 @@ class Launcher(MycroftSkill):
         self.register_entity_file("app.entity")
         #self.register_intent_file("launch.intent", self.handle_launch_intent)
         #self.apps = self.settings
+
+    def equivilency(self, app_name):
+        if app_name in {"web browser", "browser", "google", "google machine"}:
+            return "browser"
+        else:
+            return app_name
+
+    def get_target_app(self, app_title):
+        app_name = self.equivilency(app_title)
+        white_list = self.settings.get("white list").split(",")
+        if app_title not in white_list  and app_title in self.settings.keys():
+            return self.settings.get(app_title)
+        #elif app_title not in white_list and app_title not in self.settings.keys():
+        #    return 1
+        elif app_title in white_list:
+            return app_title
+        else:
+            return 1
         
     @intent_handler("launch.intent")
     def handle_launch_intent(self, app):
-        application = self.settings.get(app.data.get("app").replace("web browser", "browser"))
+        application = self.settings.get(self.equivilency(app.data.get("app")))
         self.acknowledge()
-        try:
-            run(application)
-        except:
-            run(f'echo "got error when running :  {app.__dict__}\nsettings :  {type(self.settings)}\nsettings :  {self.settings}" > ~/mycroft_launcher_error.txt')
-            run('notify-send "Mycroft" "Error opening application. Do you have it installed? Was it spelled correctly? Check ~/mycroft_launcher_error.txt for more details." -t 5000')
+        if application != 1:
+            try:
+                run(application)
+            except:
+                run(f'echo "got error when running :  {app.__dict__}\nsettings :  {type(self.settings)}\nsettings :  {self.settings}" > ~/mycroft_launcher_error.txt')
+                run('notify-send "Mycroft" "Error opening application. Do you have it installed? Was it spelled correctly whitelisted? Check ~/mycroft_launcher_error.txt for more details." -t 5000')
+        else:
+            run('notify-send "Mycroft" "I can\'t run that!"')
+            self.speak_dialog("unknown_app")
             
     def stop(self):
         pass
