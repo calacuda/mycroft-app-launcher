a060943d80bcda73d81dcd582328899147a3ab4e
calacuda <eowest@gmail.com>
2020-10-07 04:43:54 -0400

    alpha

--- __init__.py
+++ __init__.py
@@ -17,40 +17,17 @@ class Launcher(MycroftSkill):
         
     @intent_handler("launch.intent")
     def handle_launch_intent(self, app):
-        #self.speak(f"app is {app}")
-        #print("app : ", app)
-        #run(f'echo "msg-type {app.msg_type}" >> ~/out.txt')
-        #run(f'echo "data {app.data}" >> ~/out.txt')
-        #run(f'echo "context {app.context}" >> ~/out.txt')
-        #a = "app"
-        # run(f'echo "{self.settings}" > ~/settings.txt')
-        #try:
-        #    run(f'echo "apps :  {apps}" > ~/apps.txt')
-        #except:
-        #    run(f'echo "apps not assigned" > ~/apps.txt')
-        #application = "sterminal"
-        #try:
-            #application = self.apps.get(app)
-        #    application = "urxvt"
-        #except:
-        #    run(f'echo "application is sterminal" >> ~/apps.txt')
-        #    application = "sterminal"
-        #self.speak(f"launching {application}")
-        application = self.settings.get(app.data.get("app"))
+        application = self.settings.get(app.data.get("app").replace("web browser", "browser"))
         self.acknowledge()
         try:
             run(application)
         except:
-            run(f'echo "got error when running :  {app.__dict__}\nsettings :  {type(self.settings)}\nsettings :  {self.settings}" > ~/apps.txt')
-            run("wireshark")
-        self.acknowledge()
+            run(f'echo "got error when running :  {app.__dict__}\nsettings :  {type(self.settings)}\nsettings :  {self.settings}" > ~/mycroft_launcher_error.txt')
+            run('notify-send "Mycroft" "Error opening application. Do you have it installed? Was it spelled correctly? Check ~/mycroft_launcher_error.txt for more details." -t 5000')
             
     def stop(self):
         pass
 
 
 def create_skill():
-    #Launcher().handle_launch_intent("literally fucking anything")
     return Launcher()
-
-#create_skill()
